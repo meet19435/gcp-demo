49ba17bc54e90a358baee0f3760c470ee1aff77b
meet19435 <55681346+meet19435@users.noreply.github.com>
2023-02-22 13:47:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,32 @@ import requests
 
 app = Flask(__name__, static_folder="assets", template_folder="templates")
 
+# @app.route("/")
+# def k():
+# 	return render_template("index.html")
+
+# @app.route("/<a>")
+# def k1(a):
+# 	t=a+".html"
+# 	return render_template(t)
+
+# @app.route("/submit",methods=["POST"])
+# def k2():
+# 	k="\n\nEmail: "+request.form["email"]+"\nName: "+request.form["name"]+"\nPhone number: "+request.form["phone"]+"\nMessage: "+request.form["message"]
+# 	open("contact/contact.txt","a").write(k)
+# 	return render_template("contact.html")
+
+# @app.errorhandler(404)
+# def k3():
+# 	return render_template("err.html")
+
+# @app.errorhandler(Exception)
+# def k4(Exception):
+# 	return render_template("err_5.html")
+
 @app.route("/")
-def k():
-	return render_template("index.html")
+def homePage():
+	return "<h1>Hello World</h1>"
 
-@app.route("/<a>")
-def k1(a):
-	t=a+".html"
-	return render_template(t)
-
-@app.route("/submit",methods=["POST"])
-def k2():
-	k="\n\nEmail: "+request.form["email"]+"\nName: "+request.form["name"]+"\nPhone number: "+request.form["phone"]+"\nMessage: "+request.form["message"]
-	open("contact/contact.txt","a").write(k)
-	return render_template("contact.html")
-
-@app.errorhandler(404)
-def k3():
-	return render_template("err.html")
-
-@app.errorhandler(Exception)
-def k4(Exception):
-	return render_template("err_5.html")
 if __name__ == '__main__':
-	app.run(host = "0.0.0.0",port = 8080,debug = True)
+	app.run(host = "0.0.0.0",port = 8080)
